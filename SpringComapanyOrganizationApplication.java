2d4c390cb712a79cb884632e169bd41dea5630a9
varshakumre8080 <123441994+varshakumre8080@users.noreply.github.com>
2023-01-24 11:50:23 +0530

    Add files via upload

---
+++ SpringComapanyOrganizationApplication.java
@@ -0,0 +1,13 @@
+package com.ComapanyOrganization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComapanyOrganizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComapanyOrganizationApplication.class, args);
+	}
+
+}
